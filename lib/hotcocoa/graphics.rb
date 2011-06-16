ed26d80840df01b77e07e28e3ebe74ab5d496146
Mark Rada <mrada@marketcircle.com>
2011-06-16 00:57:38 -0400

    Tweak header documentation for HotCocoa::Graphics

--- lib/hotcocoa/graphics.rb
+++ lib/hotcocoa/graphics.rb
@@ -1,18 +1,26 @@
-# Ruby Cocoa Graphics is a graphics library providing a simple object-oriented 
-# interface into the power of Mac OS X's Core Graphics and Core Image drawing libraries.  
-# With a few lines of easy-to-read code, you can write scripts to draw simple or complex 
-# shapes, lines, and patterns, process and filter images, create abstract art or visualize 
-# scientific data, and much more.
-# 
-# Inspiration for this project was derived from Processing and NodeBox.  These excellent 
-# graphics programming environments are more full-featured than RCG, but they are implemented 
-# in Java and Python, respectively.  RCG was created to offer similar functionality using 
+
+# needed in case this is required without hotcocoa
+framework 'Cocoa'
+module HotCocoa; end
+
+##
+# Ruby Cocoa Graphics is a graphics library providing a simple
+# object-oriented interface into the power of Mac OS X's Core Graphics
+# and Core Image drawing libraries. With a few lines of easy-to-read
+# code, you can write scripts to draw simple or complex shapes, lines,
+# and patterns, process and filter images, create abstract art or
+# visualize scientific data, and much more.
+#
+# Inspiration for this project was derived from Processing and NodeBox.
+# These excellent graphics programming environments are more
+# full-featured than RCG, but they are implemented in Java and Python,
+# respectively.  RCG was created to offer similar functionality using
 # the Ruby programming language.
 #
-# Author::    James Reynolds  (mailto:drtoast@drtoast.com)
-# Copyright:: Copyright (c) 2008 James Reynolds
-# License::   Distributes under the same terms as Ruby 
-
+# @author    James Reynolds  [drtoast@drtoast.com](mailto:drtoast@drtoast.com)
+# @copyright Copyright (c) 2008 James Reynolds
+# @license Distributes under the same terms as Ruby
+#
 # More information about Quartz 2D is available on the Apple's website:
 # http://developer.apple.com/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_overview/dq_overview.html#//apple_ref/doc/uid/TP30001066-CH202-TPXREF101
 
