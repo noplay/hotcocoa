baa3b3ad0619d7e1f547db7dc75f2d83942004c4
Mark Rada <mrada@marketcircle.com>
2011-06-16 23:42:54 -0400

    Indent less, save trees

--- lib/hotcocoa/mappings.rb
+++ lib/hotcocoa/mappings.rb
@@ -1,150 +1,145 @@
-module HotCocoa
+module HotCocoa::Mappings
 
-  module Mappings
+  ##
+  # Load __EVERYTHING__ in `lib/hotcocoa/mappings`, recursively
+  def self.reload
+    mappings = File.join(File.dirname(__FILE__), 'mappings', '**', '*.rb')
+    Dir.glob(mappings).each { |mapping| require mapping.chomp!('.rb') }
+  end
 
-    ##
-    # Load __EVERYTHING__ in `lib/hotcocoa/mappings`, recursively
-    def self.reload
-      mappings = File.join(File.dirname(__FILE__), 'mappings', '**', '*.rb')
-      Dir.glob(mappings).each { |mapping| require mapping.chomp!('.rb') }
-    end
-
-    ##
-    # This module should be mixed into mappings that want to support
-    # target actions.
-    module TargetActionConvenience
-
-      ##
-      # @todo testing
-      def on_action= behavior
-        if target && (
-            target.instance_variable_get(:@action_behavior) ||
-              target.instance_variable_get(:@double_action_behavior))
-
-          @object.instance_variable_set(:@action_behavior, behavior)
-          @object = target
-
-        else
-          @object = Object.new
-          @object.instance_variable_set(:@action_behavior, behavior)
-          setTarget(@object)
-        end
-
-        def @object.perform_action sender
-          @action_behavior.call(sender)
-        end
-
-        setAction('perform_action:')
-      end
-
-      ##
-      # @todo testing
-      def on_action &behavior
-        self.on_action = behavior
-        self
-      end
-    end
-
-    # @group Creating a new mapping
+  ##
+  # This module should be mixed into mappings that want to support
+  # target actions.
+  module TargetActionConvenience
 
     ##
-    # @macro [new] HotCocoa::Mappings.map
-    #   Maps $1
-    #
-    # @method map :hotcocoa_name => cocoa_class, [framework: 'FrameworkName'] do ... end
-    #
     # @todo testing
-    # @todo Needs docs for all possible invocations and examples!
-    #
-    # Create a new mapping. There are several ways to call this method
-    # for different cases, only some are currently documented.
-    #
-    # @overload map( window: NSWindow )
-    # @overload map( window: 'NSWindow', framework: 'AppKit' )
-    #   In order to define a mapping without loading the dependant framework
-    def self.map options, &block
-      framework    = options.delete(:framework)
-      mapped_name  = options.keys.first
-      mapped_value = options.values.first
-      args         = [mapped_name, mapped_value]
+    def on_action= behavior
+      if target && (
+                    target.instance_variable_get(:@action_behavior) ||
+                    target.instance_variable_get(:@double_action_behavior))
 
-      if mapped_value.kind_of?(Class)
-        add_mapping(*args, &block)
+        @object.instance_variable_set(:@action_behavior, behavior)
+        @object = target
 
       else
-        if framework.nil? || loaded_framework?(framework)
+        @object = Object.new
+        @object.instance_variable_set(:@action_behavior, behavior)
+        setTarget(@object)
+      end
+
+      def @object.perform_action sender
+        @action_behavior.call(sender)
+      end
+
+      setAction('perform_action:')
+    end
+
+    ##
+    # @todo testing
+    def on_action &behavior
+      self.on_action = behavior
+      self
+    end
+  end
+
+  # @group Creating a new mapping
+
+  ##
+  # @macro [new] HotCocoa::Mappings.map
+  #   Maps $1
+  #
+  # @todo testing
+  # @todo Needs docs for all possible invocations and examples!
+  #
+  # Create a new mapping. There are several ways to call this method
+  # for different cases, only some are currently documented.
+  #
+  # @overload map( window: NSWindow )
+  # @overload map( window: 'NSWindow', framework: 'AppKit' )
+  #   In order to define a mapping without loading the dependant framework
+  def self.map options, &block
+    framework    = options.delete(:framework)
+    mapped_name  = options.keys.first
+    mapped_value = options.values.first
+    args         = [mapped_name, mapped_value]
+
+    if mapped_value.kind_of?(Class)
+      add_mapping(*args, &block)
+
+    else
+      if framework.nil? || loaded_framework?(framework)
+        add_constant_mapping(*args, &block)
+      else
+        on_framework(framework) do
           add_constant_mapping(*args, &block)
-        else
-          on_framework(framework) do
-            add_constant_mapping(*args, &block)
-          end
         end
       end
     end
+  end
 
-    ##
-    # @todo testing
-    #
-    # Registers `mapped_name` as a {Mapper#builder_method} for the given
-    # `mapped_value`. The `block` is used as the {Mapper#builder_method}'s
-    # body.
-    def self.add_mapping mapped_name, mapped_value, &block
-      m = Mapper.map_instances_of(mapped_value, mapped_name, &block)
-      mappings[m.builder_method] = m
-    end
+  ##
+  # @todo testing
+  #
+  # Registers `mapped_name` as a {Mapper#builder_method} for the given
+  # `mapped_value`. The `block` is used as the {Mapper#builder_method}'s
+  # body.
+  def self.add_mapping mapped_name, mapped_value, &block
+    m = Mapper.map_instances_of(mapped_value, mapped_name, &block)
+    mappings[m.builder_method] = m
+  end
 
-    ##
-    # @todo testing
-    #
-    # Registers `mapped_name` as a {Mapper#builder_method} for the given
-    # `constant` string which will be looked up. The `block` is used as
-    # the {Mapper#builder_method}'s body.
-    def self.add_constant_mapping mapped_name, constant, &block
-      add_mapping(mapped_name, Object.full_const_get(constant), &block)
-    end
+  ##
+  # @todo testing
+  #
+  # Registers `mapped_name` as a {Mapper#builder_method} for the given
+  # `constant` string which will be looked up. The `block` is used as
+  # the {Mapper#builder_method}'s body.
+  def self.add_constant_mapping mapped_name, constant, &block
+    add_mapping(mapped_name, Object.full_const_get(constant), &block)
+  end
 
-    ##
-    # Returns the Hash of mappings.
-    def self.mappings
-      @mappings ||= {}
-    end
+  ##
+  # Returns the Hash of mappings.
+  def self.mappings
+    @mappings ||= {}
+  end
 
-    # @group Lazy framework loading
+  # @group Lazy framework loading
 
-    ##
-    # Registers a callback for after the specified framework has been
-    # loaded.
-    def self.on_framework name, &block
-      (frameworks[name.to_s] ||= []) << block
-    end
+  ##
+  # Registers a callback for after the specified framework has been
+  # loaded.
+  def self.on_framework name, &block
+    (frameworks[name.to_s] ||= []) << block
+  end
 
-    ##
-    # Returns the Hash of mapped frameworks.
-    def self.frameworks
-      @frameworks ||= {}
-    end
+  ##
+  # Returns the Hash of mapped frameworks.
+  def self.frameworks
+    @frameworks ||= {}
+  end
 
-    ##
-    # Registers a given framework as being loaded.
-    def self.framework_loaded
-      frameworks.keys.each do |key|
-        if loaded_framework?(key)
-          frameworks[key].each { |mapper| mapper.call }
-          frameworks.delete(key)
-        end
+  ##
+  # Registers a given framework as being loaded.
+  def self.framework_loaded
+    frameworks.keys.each do |key|
+      if loaded_framework?(key)
+        frameworks[key].each { |mapper| mapper.call }
+        frameworks.delete(key)
       end
     end
+  end
 
-    ##
-    # Returns whether or not the framework has been loaded.
-    def self.loaded_framework? name
-      NSBundle.allFrameworks.map { |bundle|
-        bundle.bundlePath.split('/').last
-      }.select { |framework|
-        framework.split('.')[1] == 'framework'
-      }.map { |framework|
-        framework.split('.')[0]
-      }.include?(name.to_s)
-    end
+  ##
+  # Returns whether or not the framework has been loaded.
+  def self.loaded_framework? name
+    NSBundle.allFrameworks.map { |bundle|
+      bundle.bundlePath.split('/').last
+    }.select { |framework|
+      framework.split('.')[1] == 'framework'
+    }.map { |framework|
+      framework.split('.')[0]
+    }.include?(name.to_s)
   end
 end
