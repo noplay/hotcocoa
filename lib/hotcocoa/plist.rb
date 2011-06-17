413d14347d8c9a5d517b0e4420adb29a8242d351
Mark Rada <mrada@marketcircle.com>
2011-06-17 00:20:17 -0400

    Remove obsolete HotCocoa#read_plist method
    The method provides no advantages over the Kernel#load_plist method that MacRuby
    provides by default.
    
    The extra work it was doing for mutability is useless according to documentation.

--- lib/hotcocoa/plist.rb
+++ lib/hotcocoa/plist.rb
@@ -1,33 +1,3 @@
-module HotCocoa
-
-  def read_plist(data, mutability=:all)
-    mutability = case mutability
-      when :none
-        NSPropertyListImmutable
-      when :containers_only
-        NSPropertyListMutableContainers
-      when :all
-        NSPropertyListMutableContainersAndLeaves
-      else
-        raise ArgumentError, "invalid mutability `#{mutability}'"
-    end
-
-    if data.is_a?(String)
-      data = data.dataUsingEncoding(NSUTF8StringEncoding)
-      if data.nil?
-        raise ArgumentError, "cannot convert string `#{data}' to data"
-      end
-    end
-
-    # TODO retrieve error description and raise it if there is an error.
-    NSPropertyListSerialization.propertyListFromData(data,
-                                    mutabilityOption:mutability,
-                                              format:nil,
-                                    errorDescription:nil)
-  end
-end
-
-
 module Kernel
 
   ##
