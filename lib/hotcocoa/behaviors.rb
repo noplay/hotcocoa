1654a3b116856b1e00916c83d95552c851f6ccc0
Mark Rada <mrada@marketcircle.com>
2011-06-17 00:46:05 -0400

    Fix documentation for HotCocoa::Behaviors

--- lib/hotcocoa/behaviors.rb
+++ lib/hotcocoa/behaviors.rb
@@ -1,16 +1,31 @@
-module HotCocoa
+##
+# A mixin module for classes that subclass Cocoa classes and want to alse
+# inherit HotCocoa customizations (custom methods, constants, etc.)
+module HotCocoa::Behaviors
 
-  module Behaviors
-
-    ##
-    # Implements the callback Module#included to make sure that classes
-    # that mix in the {Behaviors} module and are themselves mixed in
-    # later will also have custom methods, delegates, etc. properly
-    # set up.
-    def self.included klass
-      Mappings::Mapper.map_class(klass)
-    end
-
+  ##
+  # Implements the callback Module#included; this method declares that when
+  # HotCocoa::Behaviors is mixed in to a class, that class will gain all the
+  # HotCocoa customizations made to classes that the class inherits from.
+  #
+  # @example
+  #
+  #   class CustomView < NSView
+  #     def begin
+  #       enter_full_screen # => NoMethodError
+  #     end
+  #   end
+  #
+  #   class OtherCustomView < NSView
+  #     include HotCocoa::Behaviors
+  #
+  #     def begin
+  #       enter_full_screen # => goes full screen
+  #     end
+  #   end
+  #
+  def self.included klass
+    Mappings::Mapper.map_class(klass)
   end
 
 end
