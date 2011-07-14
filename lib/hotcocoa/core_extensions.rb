36babdee65e05223c0ed56aaf0c10db9790a7fb7
Mark Rada <mrada@marketcircle.com>
2011-07-14 01:28:52 -0400

    Actually load the core_extensions

--- lib/hotcocoa/core_extensions.rb
+++ lib/hotcocoa/core_extensions.rb
@@ -1,4 +1,4 @@
 require 'hotcocoa/core_extensions/object'
 require 'hotcocoa/core_extensions/kernel'
-require 'hotcocoa/core_extensions/url'
+require 'hotcocoa/core_extensions/nsurl'
 require 'hotcocoa/core_extensions/nsarray'
