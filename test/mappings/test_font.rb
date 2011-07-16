79d691365e2797594e64578c49b07ecd374cfb5e
Mark Rada <mrada@marketcircle.com>
2011-07-16 00:59:43 -0400

    Actually update the tests for NSFont

--- test/mappings/test_font.rb
+++ test/mappings/test_font.rb
@@ -4,8 +4,8 @@ class TestFontMappings < MiniTest::Unit::TestCase
     assert_equal NSFont.labelFontOfSize(10), HotCocoa.font(label: 10)
     assert_equal NSFont.systemFontOfSize(15), HotCocoa.font(system: 15)
     assert_equal NSFont.controlContentFontOfSize(12), HotCocoa.font(control_content: 12)
-    assert_equal NSFont.toolTipFontsOfSize(20), HotCocoa.font(tool_tip: 20)
-    assert_equal NSFont.paletteFontOfSize(15), HotCocoa.font(palette: 32)
+    assert_equal NSFont.toolTipsFontOfSize(20), HotCocoa.font(tool_tip: 20)
+    assert_equal NSFont.paletteFontOfSize(32), HotCocoa.font(palette: 32)
   end
 
   def test_size
