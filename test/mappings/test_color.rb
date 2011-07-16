70b78172e2612fa9e8226505f224627c3966661e
Mark Rada <mrada@marketcircle.com>
2011-07-16 00:55:03 -0400

    Better implementation of existing colour tests

--- test/mappings/test_color.rb
+++ test/mappings/test_color.rb
@@ -1,15 +1,9 @@
 class TestColorMappings < MiniTest::Unit::TestCase
 
-  def rgb_test name, *components
-    color = HotCocoa.color name: name
-    [:redComponent, :greenComponent, :blueComponent].each do |component|
-      assert_equal components.shift, color.send(component), [name, component]
-    end
-  end
-
   def test_given_a_name
-    rgb_test 'red', 1, 0, 0
-    rgb_test 'green', 0, 1, 0
+    assert_equal NSColor.redColor, HotCocoa.color(name: 'red')
+    assert_equal NSColor.greenColor, HotCocoa.color(name: 'green')
+    assert_equal NSColor.lightGrayColor, HotCocoa.color(name: 'lightGray')
   end
 
 end
