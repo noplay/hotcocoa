7116b36e443efe22ff22a5c4dd3b4c0fba74800f
Mark Rada <mrada@marketcircle.com>
2011-07-23 20:19:30 -0400

    Fix up movie view in the Demo app
    Mostly just changing the video url to something that currently works.

--- examples/demo/lib/views/movie_view.rb
+++ examples/demo/lib/views/movie_view.rb
@@ -1,19 +1,21 @@
 class MovieViewDemo
 
-  def self.description 
-    "Movies" 
+  MOVIE_URL = 'http://trailers.apple.com/movies/wb/harrypotterandthedeathlyhallowspart2/hp7part2-tlr2_720p.mov'
+
+  def self.description
+    'Movies'
   end
-  
+
   def self.create
-    layout_view :frame => [0, 0, 0, 0], :layout => {:expand => [:width, :height]}, :margin => 0, :spacing => 0 do |view|
-      mview = movie_view :layout => {:expand =>  [:width, :height]},
-                         :movie => movie(:url => "http://movies.apple.com/movies/disney/wall-e/wall-e-tlr3_h.640.mov"),
-                         :controller_buttons => [:back, :volume],
-                         :fill_color => color(:name => :black)
+    layout_view frame: CGRectZero, layout: { expand: [:width, :height] }, margin: 0, spacing: 0 do |view|
+      mview = movie_view layout: { expand: [:width, :height] },
+                         controller_buttons: [:back, :volume],
+                         fill_color: color(name: 'black'),
+                         movie: movie(url: MOVIE_URL)
       view << mview
     end
   end
 
-  DemoApplication.register(self)
-  
+  DemoApplication.register self
+
 end
