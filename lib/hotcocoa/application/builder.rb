8e647013c050d0d7eac36309d0f3c6c0f7ba0b0b
Mark Rada <mrada@marketcircle.com>
2011-10-05 11:47:29 -0400

    Documentation for the Application::Builder class

--- lib/hotcocoa/application/builder.rb
+++ lib/hotcocoa/application/builder.rb
@@ -9,6 +9,9 @@ module Application
   # This class is responsible for building application bundles, but could
   # theoretically be used to build other bundles, such as frameworks, with
   # only a few changes.
+  #
+  # It is designed to work in conjunction with an {Application::Specification}
+  # object which would provide details on how to build the bundle.
   class Builder
 
     ##
@@ -22,17 +25,20 @@ module Application
       new(spec).build(opts)
     end
 
+    ##
+    # Cached spec.
+    #
     # @return [Application::Specification]
     attr_reader :spec
 
     # @param [Application::Specification]
     def initialize spec
       @spec = case spec
-      when Specification
-        spec
-      when String
-        Specification.load spec
-      end
+              when Specification
+                spec
+              when String
+                Specification.load spec
+              end
     end
 
     # @param [Hash]
@@ -50,10 +56,15 @@ module Application
       deploy if opts[:deploy]
     end
 
+    ##
+    # Run the bundle's binary directly so `STDOUT` and `STDERR` appear in the
+    # terminal.
     def run
       `"./#{executable_file}"`
     end
 
+    ##
+    # Destroy the existing bundle, if it exists.
     def remove_bundle_root
       FileUtils.rm_rf bundle_root if File.exist?(bundle_root)
     end
@@ -62,11 +73,13 @@ module Application
     private
 
     ##
-    # Build arguments list and call to `macruby_deploy`.
+    # Call `macruby_deploy` to lend a helping hand.
     def deploy
       puts `macruby_deploy --embed --gem hotcocoa #{deploy_options} #{bundle_root}`
     end
 
+    ##
+    # Build the options list to pass to `macruby_deploy`.
     def deploy_options
       options = []
       spec.gems.each { |g| options << "--gem #{g}" }
@@ -76,12 +89,16 @@ module Application
       options.join(' ')
     end
 
+    ##
+    # Setup the basic directory structure of a bundle.
     def build_bundle_structure
       [bundle_root, contents_root, frameworks_root, macos_root, resources_root].each do |dir|
         Dir.mkdir(dir) unless File.exist?(dir)
       end
     end
 
+    ##
+    # Setup the remaining standard files for the bundle.
     def write_bundle_files
       write_pkg_info_file
       write_info_plist_file
@@ -89,6 +106,8 @@ module Application
       write_ruby_main
     end
 
+    ##
+    # Copy the sources, usually just the ruby source code, into the bundle.
     def copy_sources
       spec.sources.each do |source|
         destination = File.join(resources_root, source)
@@ -97,6 +116,12 @@ module Application
       end
     end
 
+    ##
+    # @todo An example project that uses a `xib`.
+    #
+    # Copy the resources, such as images and data, into the bundle.
+    # Resources can also include interface builder files, which will
+    # be compiled for you.
     def copy_resources
       spec.resources.each do |resource|
         destination = File.join(resources_root, resource.split('/')[1..-1].join('/'))
@@ -111,20 +136,33 @@ module Application
       end
     end
 
+    ##
+    # Compile any CoreData model files and copy them to the bundle.
     def compile_data_models
       spec.data_models.each do |data|
         `/Developer/usr/bin/momc #{data} #{resources_root}/#{File.basename(data, ".xcdatamodel")}.mom`
       end
     end
 
+    ##
+    # Copy the icon file to the bundle.
     def copy_icon_file
       FileUtils.cp spec.icon, icon_file
     end
 
+    ##
+    # Generate the `PkgInfo` file for the bundle. Every bundle needs this
+    # in order identify its type of bundle and signature.
     def write_pkg_info_file
       File.open(pkg_info_file, 'wb') { |f| f.write "#{spec.type}#{spec.signature}" }
     end
 
+    ##
+    # @todo Development region needs to be configurable in the future. And
+    #       so should the default class. They should still both have defaults.
+    #
+    # Generate and write the `Info.plist` for the bundle using fields from the
+    # cached app spec.
     def write_info_plist_file
       # http://developer.apple.com/library/mac/#documentation/General/Reference/InfoPlistKeyReference/Articles/AboutInformationPropertyListFiles.html%23//apple_ref/doc/uid/TP40009254-SW1
       info = {
@@ -145,7 +183,11 @@ module Application
       File.open(info_plist_file, 'w') { |f| f.write info.to_plist }
     end
 
+    ##
     # @todo something better than puts `gcc`
+    #
+    # Create the standard bootstrap binary to launch a MacRuby app and place
+    # it in the bundle.
     def build_executable
       File.open(objective_c_source_file, 'wb') do |f|
         f.write %{
@@ -163,7 +205,9 @@ module Application
       File.unlink(objective_c_source_file)
     end
 
-    # Borrow rb_main from MacRuby Xcode templates
+    ##
+    # Borrow `rb_main` from MacRuby Xcode templates and use it to load all
+    # the sources in the bundle at boot time.
     def write_ruby_main
       File.open(main_ruby_source_file, 'wb') do |f|
         f.write <<-EOF
@@ -186,50 +230,75 @@ module Application
       end
     end
 
+    ##
+    # Path where to put the bundle.
     def bundle_root
       "#{spec.name}.app"
     end
 
+    ##
+    # Path where to put the `Contents' directory of the bundle.
     def contents_root
       File.join(bundle_root, 'Contents')
     end
 
+    ##
+    # Path where to put the `Frameworks' directory of the bundle.
     def frameworks_root
       File.join(contents_root, 'Frameworks')
     end
 
+    ##
+    # Path where to put the `MacOS' directory of the bundle.
     def macos_root
       File.join(contents_root, 'MacOS')
     end
 
+    ##
+    # Path where to put the `Resources' directory of the bundle.
     def resources_root
       File.join(contents_root, 'Resources')
     end
 
+    ##
+    # Path where to put the `Info.plist' directory of the bundle.
     def info_plist_file
       File.join(contents_root, 'Info.plist')
     end
 
+    ##
+    # Path where to put the icon for the bundle.
     def icon_file
       File.join(resources_root, "#{spec.name}.icns")
     end
 
+    ##
+    # Path where to put the `PkgInfo` file for the bundle.
     def pkg_info_file
       File.join(contents_root, 'PkgInfo')
     end
 
+    ##
+    # Generate the name of the binary. Done by removing whitespace from
+    # the `name` attribute of the cached app spec.
     def executable_file_name
       spec.name.gsub(/\s+/, '')
     end
 
+    ##
+    # Path where to put the binary file for the bundle.
     def executable_file
       File.join(macos_root, executable_file_name)
     end
 
+    ##
+    # Temporary path where the temporary source for bootstrap binary.
     def objective_c_source_file
       File.join(macos_root, 'main.m')
     end
 
+    ##
+    # Path where to put the `rb_main.rb` bootstrap file.
     def main_ruby_source_file
       File.join(resources_root, 'rb_main.rb')
     end
