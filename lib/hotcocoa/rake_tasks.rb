f8f7172ad668e7251c6e15d485f9d74667f68d18
benilovj <benilov@gmail.com>
2011-10-16 22:08:12 +0200

    rake tasks assume that there exists an appspec file in the project's root and pick it up

--- lib/hotcocoa/rake_tasks.rb
+++ lib/hotcocoa/rake_tasks.rb
@@ -1,30 +1,35 @@
 require 'hotcocoa/application/builder'
 
-builder = Application::Builder.new APPSPEC
+task :load_builder_from_appspec do
+  appspec_files = FileList["#{Rake.application.original_dir}/*.appspec"]
+  raise "No .appspec file found in your application directory!" if appspec_files.empty?
+  raise "Multiple .appspec files found in your application directory! Only one should exist." if appspec_files.size > 1
+  @builder = Application::Builder.new appspec_files.first
+end
 
 desc 'Build the application'
-task :build do
-  builder.build
+task :build => :load_builder_from_appspec do
+  @builder.build
 end
 
 desc 'Build a deployable version of the application'
-task :deploy do
-  builder.build deploy: true
+task :deploy => :load_builder_from_appspec do
+  @builder.build deploy: true
 end
 
 desc 'Build and execute the application'
 task :run => [:build] do
-  builder.run
+  @builder.run
 end
 
 desc 'Cleanup build files'
-task :clean do
-  builder.remove_bundle_root
+task :clean => :load_builder_from_appspec do
+  @builder.remove_bundle_root
 end
 
 desc 'Create the dmg archive from the application bundle'
 task :dmg => :deploy do
-  app_name = builder.spec.name
+  app_name = @builder.spec.name
   rm_rf "#{app_name}.dmg"
   sh "hdiutil create #{app_name}.dmg -quiet -srcdir #{app_name}.app -format UDZO -imagekey zlib-level=9"
 end
