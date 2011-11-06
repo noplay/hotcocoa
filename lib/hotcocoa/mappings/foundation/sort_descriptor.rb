a4c174798bde8573e824f592e0b31fd097a61e52
Mark Rada <mrada@marketcircle.com>
2011-11-05 23:41:19 -0400

    Fix sort_descriptor mapping initialization

--- lib/hotcocoa/mappings/foundation/sort_descriptor.rb
+++ lib/hotcocoa/mappings/foundation/sort_descriptor.rb
@@ -5,12 +5,11 @@ HotCocoa::Mappings.map sort_descriptor: NSSortDescriptor do
   def init_with_options sort_descriptor, opts
     if opts.has_key? :selector
       sort_descriptor.initWithKey opts.delete(:key),
-                       ascending: opts.delete(:ascending)
-
-    else
-      sort_descriptor.initWithKey opts.delete(:key),
                        ascending: opts.delete(:ascending),
                         selector: opts.delete(:selector)
+    else
+      sort_descriptor.initWithKey opts.delete(:key),
+                       ascending: opts.delete(:ascending)
     end
   end
 
