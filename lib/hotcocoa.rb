eff8f230d0b3ebd1c66213dca4d04e149f763f1a
Mark Rada <mrada@marketcircle.com>
2011-11-13 17:50:52 -0500

    Don't delegate loading classes to another file
    Another file that exists solely for loading other files seems like a bad idea. It makes
    things a little bit more organized, but at the cost of startup time. I don't think 
    HotCocoa is big enough that organization is a problem.

--- lib/hotcocoa.rb
+++ lib/hotcocoa.rb
@@ -8,7 +8,20 @@ STDOUT.reopen(IO.for_fd(NSFileHandle.fileHandleWithStandardError.fileDescriptor.
 module HotCocoa; end
 
 require 'hotcocoa/version'
-require 'hotcocoa/core_extensions'
+
+# Load extensions to Ruby core classes
+require 'hotcocoa/core_extensions/object'
+require 'hotcocoa/core_extensions/kernel'
+require 'hotcocoa/core_extensions/nsurl'
+require 'hotcocoa/core_extensions/nsarray'
+require 'hotcocoa/core_extensions/numeric'
+require 'hotcocoa/core_extensions/range'
+require 'hotcocoa/core_extensions/nsstring'
+require 'hotcocoa/core_extensions/nsattributed_string'
+require 'hotcocoa/core_extensions/nsmutable_attributed_string'
+require 'hotcocoa/core_extensions/class'
+
+# Load general HotCocoa stuff
 require 'hotcocoa/mappings'
 require 'hotcocoa/target_action_convenience'
 require 'hotcocoa/behaviors'
@@ -17,8 +30,13 @@ require 'hotcocoa/mapper'
 require 'hotcocoa/layout_view'
 require 'hotcocoa/delegate_builder'
 require 'hotcocoa/notification_listener'
-require 'hotcocoa/data_sources'
-require 'hotcocoa/kvo_accessors'
 require 'hotcocoa/attributed_string_helpers'
+require 'hotcocoa/kvo_accessors'
 
+# Load HotCocoa CoreData helper classes
+require 'hotcocoa/data_sources/table_data_source'
+require 'hotcocoa/data_sources/combo_box_data_source'
+require 'hotcocoa/data_sources/outline_view_data_source'
+
+# Force loading of mappings for loaded frameworks
 HotCocoa::Mappings.reload
--- lib/hotcocoa/data_sources.rb
+++
@@ -1,3 +0,0 @@
-require 'hotcocoa/data_sources/table_data_source'
-require 'hotcocoa/data_sources/combo_box_data_source'
-require 'hotcocoa/data_sources/outline_view_data_source'
