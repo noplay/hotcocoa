206048476cb81915db3511eea33f4cdd8a4a56fa
Jake Benilov <benilov@gmail.com>
2011-12-01 00:52:50 +0100

    split the method delegation into own builder class, added tests

--- lib/hotcocoa/delegate_builder.rb
+++ lib/hotcocoa/delegate_builder.rb
@@ -2,119 +2,124 @@
 # Builds a delegate for a control at runtime by creating a generic object
 # and adding singleton methods for each given delegate method; it then
 # tells the control to delegate to that created object.
-class HotCocoa::DelegateBuilder
+module HotCocoa
+  class DelegateBuilder
+    # @return The object that needs a delegate
+    attr_reader :control
 
-  # @return The object that needs a delegate
-  attr_reader :control
+    # @return [Array<>] Delegate methods which are assumed to be implemented
+    #    and therefore __MUST__ be given at least a stub
+    attr_reader :required_methods
 
-  # @return [Array<>] Delegate methods which are assumed to be implemented
-  #    and therefore __MUST__ be given at least a stub
-  attr_reader :required_methods
+    # @return [Object] The delegate object
+    attr_reader :delegate
 
-  # @return [Fixnum] Number of delegate methods that have been added
-  attr_reader :method_count
-
-  # @return [Object] The delegate object
-  attr_reader :delegate
-
-  # @param control the object which needs a delegate
-  # @param [Array<String>] required_methods
-  def initialize control, required_methods
-    @control = control
-    @required_methods = required_methods
-    @method_count = 0
-    @delegate = Object.new
-  end
-
-  ##
-  # Add a delegated method for {#control} to {#delegate}
-  def add_delegated_method block, selector_name, *parameters
-    clear_delegate if required_methods.empty?
-
-    @method_count += 1
-    bind_block_to_delegate_instance_variable(block)
-    create_delegate_method(selector_name, parameters)
-
-    set_delegate if required_methods.empty?
-  end
-
-  def delegate_to object, *method_names
-    method_names.each do |method_name|
-      control.send(method_name, &object.method(method_name)) if object.respond_to?(method_name)
+    # @param control the object which needs a delegate
+    # @param [Array<String>] required_methods
+    def initialize control, required_methods
+      @control = control
+      @required_methods = required_methods
+      @delegate = Object.new
     end
-  end
 
-  private
+    ##
+    # Add a delegated method for {#control} to {#delegate}
+    def add_delegated_method block, selector_name, *parameters
+      clear_delegate if required_methods.empty?
 
-  def bind_block_to_delegate_instance_variable block
-    delegate.instance_variable_set(block_instance_variable, block)
-  end
+      DelegateMethodBuilder.new(@delegate).add_delegated_method block, selector_name, *parameters
 
-  ##
-  # @todo GET RID OF EVAL
-  def create_delegate_method selector_name, parameters
-    required_methods.delete(selector_name)
-    eval %{
-      def delegate.#{parameterize_selector_name(selector_name)}
-        #{block_instance_variable}.call(#{parameter_values_for_mapping(selector_name, parameters)})
-      end
-    }
-  end
-
-  ##
-  # Reset the delegate for {#control} to `nil`.
-  def clear_delegate
-    control.setDelegate(nil) if control.delegate
-  end
-
-  ##
-  # Set the delegate for {#control} to {#delegate}
-  def set_delegate
-    control.setDelegate(delegate)
-  end
-
-  ##
-  # Returns an instance variable name to be used for the delegate method
-  # currently being built.
-  #
-  # @return [String]
-  def block_instance_variable
-    "@block#{method_count}"
-  end
-
-  ##
-  # Take an Objective-C selector and create a parameter list to be used
-  # in creating method's using #eval
-  #
-  # @example
-  #   parameterize_selector_name('myDelegateMethod') # => 'myDeletageMethod'
-  #   parameterize_selector_name('myDelegateMethod:withArgument:') # => 'myDeletageMethod p1, withArgumnet:p2'
-  #
-  # @param [String] selector_name
-  # @return [String]
-  def parameterize_selector_name selector_name
-    return selector_name unless selector_name.include?(':')
-
-    params = selector_name.split(':')
-    result = "#{params.shift} p1"
-    params.each_with_index do |param, i|
-      result << ",#{param}:p#{i + 2}"
+      required_methods.delete(selector_name)
+      set_delegate if required_methods.empty?
     end
-    result
-  end
 
-  def parameter_values_for_mapping selector_name, parameters
-    return if parameters.empty?
-
-    result = []
-    selector_params = selector_name.split(':')
-    parameters.each do |parameter|
-      if (dot = parameter.index('.'))
-        result << "p#{selector_params.index(parameter[0...dot]) + 1}#{parameter[dot..-1]}"
-      else
-        result << "p#{selector_params.index(parameter) +  1}"
+    def delegate_to object, *method_names
+      method_names.each do |method_name|
+        control.send(method_name, &object.method(method_name)) if object.respond_to?(method_name)
       end
     end
-    result.join(', ')
+
+    private
+    ##
+    # Reset the delegate for {#control} to `nil`.
+    def clear_delegate
+      control.setDelegate(nil) if control.delegate
+    end
+
+    ##
+    # Set the delegate for {#control} to {#delegate}
+    def set_delegate
+      control.setDelegate(delegate)
+    end
+  end
+
+  class DelegateMethodBuilder
+    def initialize(target)
+      @target = target
+    end
+
+    def add_delegated_method block, selector_name, *parameters
+      bind_block_to_delegate_instance_variable(selector_name, block)
+      create_delegate_method(selector_name, parameters)
+    end
+
+    def bind_block_to_delegate_instance_variable selector_name, block
+      @target.instance_variable_set(block_instance_variable_for(selector_name), block)
+    end
+
+    ##
+    # @todo GET RID OF EVAL
+    def create_delegate_method selector_name, parameters
+      eval %{
+        def @target.#{parameterize_selector_name(selector_name)}
+          #{block_instance_variable_for(selector_name)}.call(#{parameter_values_for_mapping(selector_name, parameters)})
+        end
+      }
+    end
+
+    ##
+    # Returns an instance variable name to be used for the delegate method
+    # currently being built.
+    #
+    # @return [String]
+    def block_instance_variable_for(selector_name)
+      "@block_#{selector_name.gsub(':', "_")}"
+    end
+
+    ##
+    # Take an Objective-C selector and create a parameter list to be used
+    # in creating method's using #eval
+    #
+    # @example
+    #   parameterize_selector_name('myDelegateMethod') # => 'myDeletageMethod'
+    #   parameterize_selector_name('myDelegateMethod:withArgument:') # => 'myDeletageMethod p1, withArgument:p2'
+    #
+    # @param [String] selector_name
+    # @return [String]
+    def parameterize_selector_name selector_name
+      return selector_name unless selector_name.include?(':')
+
+      params = selector_name.split(':')
+      result = "#{params.shift} p1"
+      params.each_with_index do |param, i|
+        result << ",#{param}:p#{i + 2}"
+      end
+      result
+    end
+
+    def parameter_values_for_mapping selector_name, parameters
+      return if parameters.empty?
+
+      result = []
+      selector_params = selector_name.split(':')
+      parameters.each do |parameter|
+        if (dot = parameter.index('.'))
+          result << "p#{selector_params.index(parameter[0...dot]) + 1}#{parameter[dot..-1]}"
+        else
+          result << "p#{selector_params.index(parameter) +  1}"
+        end
+      end
+      result.join(', ')
+    end
   end
 end
