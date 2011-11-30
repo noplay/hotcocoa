9c44f8055cad7d0419781044059f9d106091aa95
Jake Benilov <benilov@gmail.com>
2011-12-01 00:52:50 +0100

    split the method delegation into own builder class, added tests

--- test/test_delegate_builder.rb
+++ test/test_delegate_builder.rb
@@ -16,14 +16,28 @@ class TestDelegateBuilder < MiniTest::Unit::TestCase
   end
 
   def test_that_delegate_implements_delegate_method_with_string_parameters
-    skip("not sure whether this is failing because of a bug or the test is invalid")
     control = TestControl.new
 
-    proc = Proc.new {|p1, p2| p1.nil? ? :p1_nil : p2 }
-    delegate_builder = HotCocoa::DelegateBuilder.new(control, ["p1", "p2"])
-    delegate_builder.add_delegated_method(proc, "abc:p1:p2:")
+    proc = Proc.new {|param1, param2| param1.even? ? param1 : param2 }
+    delegate_builder = HotCocoa::DelegateBuilder.new(control, [])
+    delegate_builder.add_delegated_method(proc, "abc:param1:param2:", "param1", "param2")
 
-    assert_equal :p1_nil, delegate_builder.delegate.abc(nil, p1: nil, p2: nil)
-    assert_equal :p2, delegate_builder.delegate.abc(nil, p1: :p1, p2: :p2)
+    assert_equal 2, delegate_builder.delegate.abc(nil, param1: 2, param2: 0)
+    assert_equal 0, delegate_builder.delegate.abc(nil, param1: 1, param2: 0)
+  end
+end
+
+class TestDelegateMethodBuilder < MiniTest::Unit::TestCase
+  def test_parameterize_selector_name
+    delegate_builder = HotCocoa::DelegateMethodBuilder.new(nil)
+    assert_equal "myDelegateMethod", delegate_builder.parameterize_selector_name("myDelegateMethod")
+    assert_equal "myDelegateMethod p1,withArgument:p2", delegate_builder.parameterize_selector_name("myDelegateMethod:withArgument:")
+  end
+
+  def test_parameter_values_for_mapping
+    delegate_builder = HotCocoa::DelegateMethodBuilder.new(nil)
+    assert delegate_builder.parameter_values_for_mapping("myDelegateMethod", []).nil?
+    assert_equal "p2, p3", delegate_builder.parameter_values_for_mapping("myDelegateMethod:p1:p2:", ["p1", "p2"])
+    assert_equal "p1.userInfo['NSExposedRect']", delegate_builder.parameter_values_for_mapping("windowDidExpose:", ["windowDidExpose.userInfo['NSExposedRect']"])
   end
 end
