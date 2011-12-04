33fc6dd9a9c69e7774036d9ad5985caf059facb1
Mark Rada <mrada@marketcircle.com>
2011-12-04 14:20:11 -0500

    Fix a name method name conflict with newer versions of minitest

--- test/core_extensions/test_range.rb
+++ test/core_extensions/test_range.rb
@@ -12,19 +12,19 @@ class TestRangeToNSRange < MiniTest::Unit::TestCase
     @@array ||= (0..10).to_a
   end
 
-  def run_test range, *arg
+  def range_test range, *arg
     assert_equal array.slice(range), array.subarrayWithRange(range.to_NSRange(*arg)), range
   end
 
   def test_cases
-    run_test 0..10
-    run_test 1..10
-    run_test 2..-1, array.size
-    run_test 3..-2, array.size
-    run_test 4...-1, array.size
-    run_test -3...-1, array.size
-    run_test -5..-1, array.size
-    # run_test -1..-2, array.size # returns an empty array, is a fucked up case to begin with
+    range_test 0..10
+    range_test 1..10
+    range_test 2..-1, array.size
+    range_test 3..-2, array.size
+    range_test 4...-1, array.size
+    range_test -3...-1, array.size
+    range_test -5..-1, array.size
+    # range_test -1..-2, array.size # returns an empty array, is a fucked up case to begin with
   end
 
 end
